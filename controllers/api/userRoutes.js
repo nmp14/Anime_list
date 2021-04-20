f02e894d33832c0a6148e2abcd9197622652967d
Nathan Peek <nmp14fsu@gmail.com>
2021-04-20 03:34:40 -0400

    Create login post route on server

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -32,10 +32,27 @@ router.post("/", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email });
-        console.log(user);
-        res.status(200).json(user);
+
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+            return;
+        }
+
+        const isValidPass = await user.comparePassword(req.body.password, (err, isMatch) => {
+            if (err) {
+                console.log(err)
+            } else {
+                if (!isMatch) {
+                    res.status(400).json({ message: "Invalid username or password" });
+                    return;
+                } else {
+                    res.status(200).json({ message: "Successfully logged in" });
+                }
+            }
+        });
     } catch (e) {
         res.status(500).json(e);
+        console.log(e);
     }
 });
 
