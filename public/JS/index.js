07bb84b0f8a456e007840031c75a7077ec1f4508
Nathan Peek <nmp14fsu@gmail.com>
2021-04-20 00:42:41 -0400

    Add functionality to buttons to swap forms. Add cursor points to css file

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,7 +1,12 @@
+// Nav page elements
 const navPage = document.querySelector(".nav-page");
 const navOpen = document.getElementById("navOpen");
 const navClose = document.getElementById("navClose");
+// loginPage elements
+const swapToRegister = document.getElementById("swapToRegister");
+const swapToLogin = document.getElementById("swapToLogin");
 
+// Nav page functions 
 const openNav = (e) => {
     e.preventDefault();
 
@@ -19,5 +24,24 @@ const closeNav = (e) => {
     }, 700);
 }
 
+// login/register functions
+const swapForms = () => {
+    if (swapToRegister) {
+        swapToRegister.addEventListener("click", (e) => {
+            e.preventDefault();
+            document.getElementById("loginForm").classList.add("hidden");
+            document.getElementById("registerForm").classList.remove("hidden");
+        });
+    } else if (swapToLogin) {
+        swapToLogin.addEventListener("click", (e) => {
+            e.preventDefault();
+            document.getElementById("loginForm").classList.remove("hidden");
+            document.getElementById("registerForm").classList.add("hidden");
+        });
+    }
+    swapForms();
+}
+
 if (navOpen) navOpen.addEventListener("click", openNav);
 if (navClose) navClose.addEventListener("click", closeNav);
+if (swapToLogin || swapToRegister) swapForms();
