d1245c54214d78405adc4bf4f15acf5db7cba20c
Nathan Peek <nmp14fsu@gmail.com>
2021-04-20 01:43:12 -0400

    Fix create user route to validate

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,18 +12,28 @@ router.post("/", async (req, res) => {
             password: req.body.password
         });
 
-        user.save((err, newUser) => {
+        const savedUser = await user.save(function (err, doc) {
             if (err) {
-                console.log(err);
                 res.status(400).json(err);
-                return;
+            } else {
+                res.status(201).json(doc);
             }
         });
+    } catch (e) {
+        res.status(500).json(e);
+        console.log(e);
+    }
+});
 
+// Login
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        console.log(user);
         res.status(200).json(user);
     } catch (e) {
         res.status(500).json(e);
     }
-});
+})
 
 module.exports = router;
