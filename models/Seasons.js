8b50b211eb00036b4b7651db97d989c659126113
Nathan Peek <nmp14fsu@gmail.com>
2021-04-20 23:35:47 -0400

    Create Season model

---
+++ models/Seasons.js
@@ -0,0 +1,32 @@
+const Mongoose = require("mongoose");
+
+const Schema = Mongoose.Schema;
+
+const SeasonSchema = new Schema({
+    season: {
+        type: String,
+        required: "Season is required",
+        validate: [(inputSeason) => {
+            const seasons = ["Spring", "Summer", "Fall", "winter"];
+            for (const season of seasons) {
+                if (season === inputSeason) return true;
+            }
+            return false;
+        }, "Not a valid season"]
+    },
+    year: {
+        type: Date,
+        required: "Year is required",
+        validate: [({ length }) => length === 4, "Must be length of 4 for year"],
+    },
+    anime: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Anime"
+        }
+    ]
+});
+
+const Season = Mongoose.model("Season", SeasonSchema);
+
+module.exports = Season;
