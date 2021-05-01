103e56e67fd88f4149ae5edc6fe08d7d6822f4ae
Nathan Peek <nmp14fsu@gmail.com>
2021-04-30 23:26:44 -0400

    Create routes for getting season and populating anime, adding anime, and removing anime

--- controllers/api/seasonRoutes.js
+++ controllers/api/seasonRoutes.js
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
         console.log(e);
     }
 });
-
+// Create a new season
 router.post("/", async (req, res) => {
     try {
         const season = db.Season.create({
@@ -29,6 +29,56 @@ router.post("/", async (req, res) => {
             }
             res.status(201).json(doc);
         });
+    } catch (e) {
+        res.status(500).json(e);
+        console.log(e);
+    }
+});
+//Get Season with anime
+router.get("/:id", async (req, res) => {
+    try {
+        const season = await db.Season.findOne({ _id: req.params.id })
+            .populate("anime")
+            .exec((err, doc) => {
+                if (err) {
+                    res.status(404).json({ message: err });
+                    return;
+                }
+                res.status(200).json(doc);
+            });
+    } catch (e) {
+        res.status(500).json(e);
+        console.log(e);
+    }
+})
+
+// Add anime to Season
+router.put("/addAnime", async (req, res) => {
+    try {
+        const season = await db.Season.updateOne({
+            season: req.body.season,
+            year: req.body.year
+        }, { $push: { anime: req.body.anime_id } });
+
+        res.status(200).json(season);
+    } catch (e) {
+        res.status(500).json(e);
+        console.log(e);
+    }
+});
+
+router.put("/removeAnime", async (req, res) => {
+    try {
+        const season = await db.Season.updateOne({
+            season: req.body.season,
+            year: req.body.year
+        }, { $pull: { anime: req.body.anime_id } }, (err, doc) => {
+            if (err) {
+                res.status(400).json(err);
+                return;
+            }
+            res.status(200).json(doc);
+        })
     } catch (e) {
         res.status(500).json(e);
         console.log(e);
